374637252e5aa172bc19cf6960248cc6f2a1d321
kinglearned <kinglearned@gmail.com>
2023-06-18 13:27:18 +0100

    Condiguration of config

---
+++ config.js
@@ -0,0 +1,7 @@
+// Configure the database key
+const DatabaseName = 'codebridge'
+const Host = 'localhost'
+const User = 'root'
+const Password = 'Learned 45'
+
+module.exports = { DatabaseName, Host, User, Password }
