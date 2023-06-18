edba0ad659ca81c97c47864ebf1356b566dbe723
kinglearned <kinglearned@gmail.com>
2023-06-18 18:36:53 +0100

    Modifying the config file

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Configure the database key
-const DatabaseName = 'codebridge'
-const Host = 'localhost'
-const User = 'root'
-const Password = 'Learned 1945'
+const DatabaseName = 'codebridge' //this database will be created dynamicaally from the `DBinitialization.js` file
+const Host = 'localhost' //your MySQL Server Host
+const User = 'root' //Your username
+const Password = 'Learned 1945' //Your password if any
 
 module.exports = { DatabaseName, Host, User, Password }
